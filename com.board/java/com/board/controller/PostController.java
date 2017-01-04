21478c3aac2ad39faaea96c016cec11850ba00c2
ldh <ldh@DESKTOP-HKHASKS>
2017-01-04 15:37:29 +0900

    //풀커믿

--- com.board/java/com/board/controller/PostController.java
+++ com.board/java/com/board/controller/PostController.java
@@ -1,12 +1,11 @@
 package com.board.controller;
 
-import java.sql.Date;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import antlr.collections.List;
 
 import com.board.DTO.Post;
 import com.board.service.PostService;
