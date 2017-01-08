83d54fe76068bae1bdcd99991ae8ba2ad11de0d8
LDH <lim9101@name.com>
2017-01-08 23:47:49 +0900

    //동호// comentMapper

--- com.board/java/com/board/controller/UserController.java
+++ com.board/java/com/board/controller/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
-	//commit;실험
+	
 	
 	//회원가입 기능
 	//ParameterType:User,HttpSession resultType:boolean
@@ -67,6 +67,7 @@ public class UserController {
 		return result;
 	}
 	
+	//회원정보수정
 	@RequestMapping(value="updateUser", method=RequestMethod.POST)
 	@ResponseBody
 	public boolean updateUser(User user,HttpSession session){
